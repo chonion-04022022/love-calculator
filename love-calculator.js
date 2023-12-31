ff973ce9eda90cf3c00cb9990fff5247a9195b9a
Fahim Morshed Nion <114305756+chonion-04022022@users.noreply.github.com>
2024-01-01 02:05:41 +0600

    Create love-calculator.js

---
+++ love-calculator.js
@@ -0,0 +1,4 @@
+//love calculator
+var name1= prompt("what is your name?");
+var name2= prompt("what is your lover's name?");
+alert(name1 + name2 +" you guys have a love connection of " + Math.floor((Math.random()*100)+1) + "%");
